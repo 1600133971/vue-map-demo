4a6feffa4f7919ef3314bbae3b6cd316638d5a22
1600133971 <34409521+1600133971@users.noreply.github.com>
2018-10-08 23:56:08 +0800

    0.1.3

--- src/components/amap/st-amap-azimuth.vue
+++ src/components/amap/st-amap-azimuth.vue
@@ -6,8 +6,8 @@
 
 <style lang="css">
 .st-map-azimuth {
-  width: 700px;
-  height: 500px;
+  width: 800px;
+  height: 800px;
   position: absolute;
 }
 </style>
@@ -19,18 +19,25 @@ export default {
   data() {
     return {
       halfPowerAngle: 60,
-      path: [[null], [null], [null]],
+      path: [[null], [null], [null], [null]],
       pointList: [
         [121.59996, 31.197646],
         [121.61996, 31.217646],
-        [121.63996, 31.237646]
+        [121.63996, 31.237646],
+        [121.57996, 31.237646]
       ],
       starCenter: [this.lng, this.lat],
-      angleList: [[60, 180, 300], [60, 180, 300], [60, 180, 300]],
-      radius: 50,
+      angleList: [
+        [0, 120, 240],
+        [60, 180, 300],
+        [30, 150, 240],
+        [30, 150, 240]
+      ],
+      radius: 30,
       colorList: [
         ["red", "blue", "green"],
-        ["red", "blue", "yellow"],
+        ["blue", "red", "yellow"],
+        ["red", "blue", "orange"],
         ["red", "blue", "orange"]
       ]
     };
@@ -76,10 +83,12 @@ export default {
         return (angle * Math.PI) / 180;
       };
 
+      let currentRadius = (this.radius * (this.map.getZoom() - 3)) / 12;
+
       for (var j = 0; j < this.pointList.length; j++) {
         let center = this.map.lngLatToContainer(this.pointList[j]);
 
-        for (var i = 0; i < this.angleList.length; i++) {
+        for (var i = 0; i < this.angleList[j].length; i++) {
           var starttangle = angleToRadian(
             this.angleList[j][i] - this.halfPowerAngle / 2
           );
@@ -89,10 +98,10 @@ export default {
 
           //计算出楔和园相交的两个点
           //这些计算公式都是以12点钟方向为0°,顺时针方向角度递增
-          var x1 = center.x + this.radius * Math.sin(starttangle);
-          var y1 = center.y - this.radius * Math.cos(starttangle);
-          var x2 = center.x + this.radius * Math.sin(endangle);
-          var y2 = center.y - this.radius * Math.cos(endangle);
+          var x1 = center.x + currentRadius * Math.sin(starttangle);
+          var y1 = center.y - currentRadius * Math.cos(starttangle);
+          var x2 = center.x + currentRadius * Math.sin(endangle);
+          var y2 = center.y - currentRadius * Math.cos(endangle);
 
           //这个标记表示角度大于半圆,此标记在绘制SBG弧形组件的时候需要
           var big = endangle - starttangle > Math.PI ? 1 : 0;
@@ -108,9 +117,9 @@ export default {
             "," +
             y1 + //画一条到(x1,y1)的线段
             " A " +
-            this.radius +
+            currentRadius +
             "," +
-            this.radius + //再画一条半径为r的弧
+            currentRadius + //再画一条半径为r的弧
             " 0 " +
             big +
             " 1 " + // 弧的详细信息
